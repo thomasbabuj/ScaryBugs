896e2fe345a25b5f00222f2819a977ba900e2d6b
Thomas Babu Joseph <thomasbabuj@Thomas-MacBook-Pro.local>
2013-07-18 22:03:00 +0800

    created initialiser to fill in our instance variables from the passed-in parameters.

--- Scarybugs/ScaryBugData.h
+++ Scarybugs/ScaryBugData.h
@@ -18,6 +18,7 @@
 @property (assign) float rating;
 
 
--(id)initWithTitle:(NSString *) title rating:(float) rating;
+-(id)initWithTitle:(NSString *)title
+            rating:(float)rating;
 
 @end
--- Scarybugs/ScaryBugData.m
+++ Scarybugs/ScaryBugData.m
@@ -10,4 +10,16 @@
 
 @implementation ScaryBugData
 
+@synthesize title = _title;
+@synthesize rating = _rating;
+
+-(id)initWithTitle:(NSString *)title rating:(float)rating {
+    if( self = [super init] ) {
+        self.title = title;
+        self.rating = rating;
+    }
+    
+    return self;
+}
+
 @end
