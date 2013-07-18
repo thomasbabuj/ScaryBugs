226b8ce6ab2da05161d7e4e492c5b6fc7cc4e07b
Thomas Babu Joseph <thomasbabuj@Thomas-MacBook-Pro.local>
2013-07-18 21:55:00 +0800

    Declared objects with two properties.

--- Scarybugs/ScaryBugData.h
+++ Scarybugs/ScaryBugData.h
@@ -10,4 +10,14 @@
 
 @interface ScaryBugData : NSObject
 
+// Declaring object with two properties
+// strong - ARC runtime will keep the object in memory as long as there's reference to it around, and deallocate it when no reference remain
+// assign - Property is set directly, with no memory management invloved. this is what i need to set for primitive types like float.
+//
+@property (strong) NSString *title;
+@property (assign) float rating;
+
+
+-(id)initWithTitle:(NSString *) title rating:(float) rating;
+
 @end
