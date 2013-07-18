f520386dc3624c77443535d99198dbc2a9cbe50b
Thomas Babu Joseph <thomasbabuj@Thomas-MacBook-Pro.local>
2013-07-18 22:04:35 +0800

    Added new class called ScaryBugDoc to store all the images etc  about the bugs.

---
+++ Scarybugs/ScaryBugDoc.m
@@ -0,0 +1,13 @@
+//
+//  ScaryBugDoc.m
+//  Scarybugs
+//
+//  Created by Thomas Babu Joseph on 18/7/13.
+//  Copyright (c) 2013 Thomas Babu Joseph. All rights reserved.
+//
+
+#import "ScaryBugDoc.h"
+
+@implementation ScaryBugDoc
+
+@end
