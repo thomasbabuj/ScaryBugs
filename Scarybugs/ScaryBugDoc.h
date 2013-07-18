60c39ad4de33fe238a1131ce823193adf95d32c1
Thomas Babu Joseph <thomasbabuj@Thomas-MacBook-Pro.local>
2013-07-18 22:24:19 +0800

    creating some instance variables/properties and an initializer.

--- Scarybugs/ScaryBugDoc.h
+++ Scarybugs/ScaryBugDoc.h
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+//Including the scaryBugData class
+@class ScaryBugData;
+
 @interface ScaryBugDoc : NSObject
+
+@property (strong) ScaryBugData *data;
+@property (strong) UIImage *thumbImage;
+@property (strong) UIImage *fullImage;
+
+-(id)intiWithTitle:(NSString *)title
+            rating: (float) rating
+        thumbImage: (UIImage *)thumbImage
+        fullImage : (UIImage *)fullImage;
+
 
 @end
