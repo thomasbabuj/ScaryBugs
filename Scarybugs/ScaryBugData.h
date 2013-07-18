25b14605454be6b60a2d48f1583598f64735520d
Thomas Babu Joseph <thomasbabuj@Thomas-MacBook-Pro.local>
2013-07-18 21:41:30 +0800

    Created scaryBugData class inside the Model folder

---
+++ Scarybugs/ScaryBugData.h
@@ -0,0 +1,13 @@
+//
+//  ScaryBugData.h
+//  Scarybugs
+//
+//  Created by Thomas Babu Joseph on 18/7/13.
+//  Copyright (c) 2013 Thomas Babu Joseph. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScaryBugData : NSObject
+
+@end
---
+++ Scarybugs/ScaryBugData.m
@@ -0,0 +1,13 @@
+//
+//  ScaryBugData.m
+//  Scarybugs
+//
+//  Created by Thomas Babu Joseph on 18/7/13.
+//  Copyright (c) 2013 Thomas Babu Joseph. All rights reserved.
+//
+
+#import "ScaryBugData.h"
+
+@implementation ScaryBugData
+
+@end
